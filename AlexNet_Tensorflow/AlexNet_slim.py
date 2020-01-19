13ebc1162c57c9956a52f6c4c3e53d84bccfb553
alexchung <yonganzhong@outlook.com>
2020-01-19 18:03:09 +0800

    improve alexnet

--- AlexNet_Tensorflow/AlexNet_slim.py
+++ AlexNet_Tensorflow/AlexNet_slim.py
@@ -2,42 +2,49 @@
 # -*- coding: utf-8 -*-
 #------------------------------------------------------
 # @ File       : AlexNet_slim.py
-# @ Description:
+# @ Description: reference http://www.cs.toronto.edu/~guerzhoy/tf_alexnet/
+#                          https://github.com/kratzert/finetune_alexnet_with_tensorflow
 # @ Author     : Alex Chung
 # @ Contact    : yonganzhong@outlook.com
 # @ License    : Copyright (c) 2017-2018
-# @ Time       : 2020/1/18 下午3:07
+# @ Time       : 2020/1/18 PM 3:07
 # @ Software   : PyCharm
 #-------------------------------------------------------
 
 import os
+import numpy as np
 import tensorflow as tf
 import tensorflow.contrib.slim as slim
+from tensorflow.contrib.layers import xavier_initializer, xavier_initializer_conv2d
 
 
 class AlexNet():
     """
     VGG16 model
     """
-    def __init__(self, input_shape, num_classes, batch_size, decay_rate, learning_rate,keep_prob=0.5,
-                 weight_decay=0.00005, num_samples_per_epoch=None, num_epoch_per_decay=None, is_pretrain=False):
+    def __init__(self, input_shape, num_classes, batch_size, learning_rate=0.001, keep_prob=0.5, weight_decay=0.00005,
+                 is_pretrain=False,):
         self.num_classes = num_classes
         self.batch_size = batch_size
-        self.decay_steps = int(num_samples_per_epoch / batch_size * num_epoch_per_decay)
-        self.decay_rate = decay_rate
+
         self.learning_rate = learning_rate
         # self.optimizer = optimizer
         self.keep_prob = keep_prob
         self.weight_decay = weight_decay
 
-        self.is_pretrain = is_pretrain
         self._R_MEAN = 123.68
-        self._G_MEAN = 116.78
-        self._B_MEAN = 103.94
+        self._G_MEAN = 116.779
+        self._B_MEAN = 103.939
+
+        self.is_pretrain = is_pretrain
         # self.initializer = tf.random_normal_initializer(stddev=0.1)
         # add placeholder (X,label)
         self.raw_input_data = tf.placeholder(tf.float32, shape=[None, input_shape[0], input_shape[1], input_shape[2]],
                                              name="input_images")
+        self.raw_input_data = self.mean_subtraction(self.raw_input_data, means=[self._R_MEAN,
+                                                                                self._G_MEAN,
+                                                                                self._B_MEAN])
+        self.raw_input_data = self.convert_rgb_to_bgr(self.raw_input_data)
         # y [None,num_classes]
         self.raw_input_label = tf.placeholder(tf.float32, shape=[None, self.num_classes], name="class_label")
         self.is_training = tf.compat.v1.placeholder_with_default(input=False, shape=(), name='is_training')
@@ -81,29 +88,33 @@ class AlexNet():
                 scope='alexnet'):
         with tf.variable_scope(scope, 'alexnet', [inputs]) as sc:
             with slim.arg_scope(self.alexnet_arg_scope(weight_decay=weight_decay)):
-                net = slim.conv2d(inputs, 64, [11, 11], 4, padding='VALID', scope='conv1')
-                net = slim.max_pool2d(net, [3, 3], 2, scope='pool1')
-                net = slim.conv2d(net, 192, [5, 5], scope='conv2')
-                net = slim.max_pool2d(net, [3, 3], 2, scope='pool2')
-                net = slim.conv2d(net, 384, [3, 3], scope='conv3')
-                net = slim.conv2d(net, 384, [3, 3], scope='conv4')
-                net = slim.conv2d(net, 256, [3, 3], scope='conv5')
+                # 227 * 227 * 3
+                net = self.conv_layer(inputs, 96, [11, 11], 4, padding='VALID', name='conv1')
+                # 55 * 55 * 64
+                # Local Response Normalization
+                lrn1 = tf.nn.lrn(net, depth_radius=2, alpha=2e-05, beta=0.75, bias=1.0)
+                net = slim.max_pool2d(lrn1, [3, 3], 2, padding='VALID', scope='pool1')
+
+                net = self.conv_layer(net, 256, [5, 5], groups=2, name='conv2')
+                lrn2 = tf.nn.lrn(net, depth_radius=2, alpha=2e-05, beta=0.75, bias=1.0)
+                net = slim.max_pool2d(lrn2, [3, 3], 2, scope='pool2')
+
+                net = self.conv_layer(net, 384, [3, 3], name='conv3')
+                net = self.conv_layer(net, 384, [3, 3], groups=2, name='conv4')
+
+                net = self.conv_layer(net, 256, [3, 3], groups=2, name='conv5')
                 net = slim.max_pool2d(net, [3, 3], 2, scope='pool5')
 
-                with slim.arg_scope([slim.conv2d],
-                                    weights_initializer= tf.truncated_normal_initializer(0.0, 0.005),
-                                    biases_initializer=tf.constant_initializer(0.1)):
-                    net = slim.conv2d(net, 4096, [5, 5], padding='VALID',
-                                      scope='fc6')
-                    net = slim.dropout(net, keep_prob, is_training=is_training, scope='dropout6')
+                # flatten and full connect layer
+                net = tf.reshape(net, shape=[-1, 6 * 6 * 256], name='flattened')
+                net = slim.fully_connected(net, num_outputs=4096, scope='fc6')
+                net = slim.dropout(net, keep_prob=keep_prob)
+                net = slim.fully_connected(net, num_outputs=4096, scope='fc7')
+                net = slim.dropout(net, keep_prob=keep_prob)
 
-                    net = slim.conv2d(net, 4096, [1, 1], scope='fc7')
-                    net = slim.dropout(net, keep_prob, is_training=is_training, scope='dropout7')
+                net = slim.fully_connected(net, num_outputs=num_classes, activation_fn=None, scope='fc8')
+                logits = slim.softmax(net, scope='logits')
 
-                    net = slim.conv2d(net, num_classes, [1, 1],  activation_fn=None,  normalizer_fn=None,
-                                      biases_initializer=tf.zeros_initializer(), scope='fc8')
-                    net = tf.squeeze(net, [1, 2], name='fc8/squeezed')
-                    logits = slim.softmax(logits=net, scope='softmax')
                 return logits
 
     def training(self, learnRate, globalStep):
@@ -125,15 +136,10 @@ class AlexNet():
                 variables = tf.model_variables(scope=scope)
                 [trainable_variable.append(var) for var in variables]
 
-        learning_rate = tf.train.exponential_decay(learning_rate=learnRate, global_step=globalStep,
-                                                   decay_steps=self.decay_steps, decay_rate=self.decay_rate,
-                                                   staircase=False)
         # according to use request of slim.batch_norm
         # update moving_mean and moving_variance when training
-        update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
-        with tf.control_dependencies(update_ops):
-            train_op = tf.train.AdamOptimizer(learning_rate).minimize(self.loss, global_step=globalStep,
-                                                                      var_list=trainable_variable)
+        train_op = tf.train.AdamOptimizer(learnRate).minimize(self.loss, global_step=globalStep,
+                                                              var_list=trainable_variable)
         return train_op
 
     def losses(self, logits, labels, name):
@@ -165,6 +171,78 @@ class AlexNet():
         }
         return feed_dict
 
+    def conv_layer(self, inputs, num_outputs, kernel_size, stride=1, name=None, padding='SAME', use_bias=True, groups=1):
+        """Create a convolution layer.
+        Adapted from: https://github.com/ethereon/caffe-tensorflow
+        """
+        # Get number of input channels
+        input_channels = int(inputs.get_shape()[-1])
+
+        # Create lambda function for the convolution
+        convolve = lambda inputs, kernel: tf.nn.conv2d(inputs, kernel,
+                                             strides=[1, stride, stride, 1],
+                                             padding=padding)
+
+        with tf.variable_scope(name,) as scope:
+            # Create tf variables for the weights and biases of the conv layer
+            weights = tf.get_variable('weights',
+                                      shape=[kernel_size[0], kernel_size[1], input_channels / groups, num_outputs],
+                                      initializer=xavier_initializer_conv2d(),
+                                      trainable=True)
+            # tf.add_to_collection(tf.GraphKeys.MODEL_VARIABLES, weights)
+
+            biases = tf.get_variable('biases', shape=[num_outputs])
+            # tf.add_to_collection(tf.GraphKeys.MODEL_VARIABLES, biases)
+
+        if groups == 1:
+            # net = slim.conv2d(inputs, num_outputs=num_outputs, kernel_size=kernel_size, stride=stride, padding=padding,
+            #                   scope=name)
+            net = convolve(inputs, weights)
+
+        # In the cases of multiple groups, split inputs & weights and
+        else:
+            # Split input and weights and convolve them separately
+            input_groups = tf.split(value=inputs, axis=3, num_or_size_splits=groups)
+            weight_groups = tf.split(value=weights, axis=3, num_or_size_splits=groups)
+
+            output_groups = [convolve(input, kernel) for input, kernel in zip(input_groups, weight_groups)]
+
+            # Concat the convolved output together again
+            net = tf.concat(axis=3, values=output_groups)
+        if use_bias:
+            # Add biases
+            bias = tf.reshape(tf.nn.bias_add(net, biases), tf.shape(net))
+            # Apply relu function
+            net = tf.nn.relu(bias, name=scope.name)
+
+        return net
+
+    def load_pretrain_model(self, sess, model_path):
+        """
+
+        :param sess:
+        :param model_path:
+        :return:
+        """
+        weights_dict = np.load(model_path, encoding='bytes', allow_pickle=True).item()
+        weights_dict = dict(weights_dict)
+
+        custom_scope = ['fc8']
+
+        for op_name in weights_dict:
+            if op_name not in custom_scope:
+                for data in weights_dict[op_name]:
+                    with tf.variable_scope('alexnet/' + op_name, reuse=True):
+                        # Biases
+                        if len(data.shape) == 1:
+                            var = tf.get_variable('biases', trainable=True)
+                            sess.run(var.assign(data))
+                        # Weights
+                        else:
+                            var = tf.get_variable('weights', trainable=True)
+                            sess.run(var.assign(data))
+        print('+++++++++++++++++++Successful load all variable+++++++++++++++++++')
+
     def alexnet_arg_scope(self, weight_decay=0.0005):
         with slim.arg_scope([slim.conv2d, slim.fully_connected],
                             activation_fn=tf.nn.relu,
@@ -173,5 +251,28 @@ class AlexNet():
             with slim.arg_scope([slim.conv2d], padding='SAME'):
                 with slim.arg_scope([slim.max_pool2d], padding='VALID') as arg_sc:
                     return arg_sc
+
+    def mean_subtraction(self, image, means):
+        """
+        subtract the means form each image channel (white image)
+        :param image:
+        :param mean:
+        :return:
+        """
+        num_channels = image.get_shape()[-1]
+        image = tf.cast(image, dtype=tf.float32)
+        channels = tf.split(value=image, num_or_size_splits=num_channels, axis=3)
+        for n in range(num_channels):
+            channels[n] -= means[n]
+        return tf.concat(values=channels, axis=3, name='concat_channel')
+
+    def convert_rgb_to_bgr(self, image):
+        """
+        image process
+        :param image:
+        :return:
+        """
+        return image[:, :, :, ::-1]
+
 
 
