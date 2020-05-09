2c3628253431c59879d45b1c3f7c28d69d98f9a1
alexchung <yonganzhong@outlook.com>
2020-05-09 18:05:04 +0800

    add eval module

---
+++ AlexNet_Tensorflow/tools/eval.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#------------------------------------------------------
+# @ File       : eval.py
+# @ Description:  
+# @ Author     : Alex Chung
+# @ Contact    : yonganzhong@outlook.com
+# @ License    : Copyright (c) 2017-2018
+# @ Time       : 2020/5/9 下午4:31
+# @ Software   : PyCharm
+#-------------------------------------------------------
